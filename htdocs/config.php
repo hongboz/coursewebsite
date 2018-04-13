8de353f9b9a155993787f3c8c9f884732b6aa3e5
hongboz <38359848+hongboz@users.noreply.github.com>
2018-04-13 14:19:51 -0400

    Update config.php

--- htdocs/config.php
+++ htdocs/config.php
@@ -1,7 +1,7 @@
 <?php
-   define('DB_SERVER', 'mathlab.utsc.utoronto.ca');
-   define('DB_USERNAME', 'liukev11');
-   define('DB_PASSWORD', 'liukev11');
+   define('DB_SERVER', 'localhost');
+   define('DB_USERNAME', 'root');
+   define('DB_PASSWORD', 'root');
    define('DB_DATABASE', 'a3');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
